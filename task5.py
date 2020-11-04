e054134a11b20cbeff910dd585fc6ab200a65261
00010847 <00010847F@gmail.com>
2020-11-04 11:28:13 +0500

    solved

--- task5.py
+++ task5.py
@@ -1,12 +1,9 @@
 def find(word, letter, start_index):
 
-
-
     while start_index < len(word):
         if word[start_index] == letter:
             return start_index
         start_index = start_index + 1
     return -1
 
-
-print(find("computer", "p"))
+print(find("computer", "p", 4))
